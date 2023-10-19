5d55d0510eab701b40c0831c25d8e7449f0ad173
bencebekefi <146179275+bencebekefi@users.noreply.github.com>
2023-10-19 15:41:30 +0100

    Lab work

--- Tasks/05 - Functions/Functions/503-passing-parameters/503-passing-parameters.cpp
+++ Tasks/05 - Functions/Functions/503-passing-parameters/503-passing-parameters.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-void displayBanner(int stringLen)
+void displayLineOfStars(int stringLen)
 {
 	for (unsigned int n = 0; n < (stringLen + 4); n++) {
 		cout << "*";
 	}
 	cout << endl;
 }
+//std:: string getNewMessage() {
+//	std::string newmessage;
+//
+//	std:cout <"nter your message"
+//	std: Getline(std::cin, newmessage)
+//	return newmessage
+//}
+
+std::string getNewMessage()
+{
+	std:: string newMessage;
+
+	std:: cout << "Enter your message";
+	//cin >> newMessage;
+	std::getline(std::cin, newMessage);
+	
+	return newMessage;
+
+}
+//void displayWithinBanner(string newMessage)
+//{
+//	//Code here	
+//	cin >> newMessage;
+//}
 
 int main()
 {
-	const string message = "Welcome to Computer Science";
+	//const string message = "Welcome to Computer Science";
+	string message = getNewMessage();
 
 	//Get the length of the string
 	int L = message.length();			//L is now a LOCAL variable
 
+
 	//Write the banner above
-	displayBanner(L);
+	displayLineOfStars(L);
 
 	cout << "* ";				//Leading *
 	cout << message;			//Message
+
 	cout << " *" << endl;		//Trailing *
 
 	//Write the banner below
-	displayBanner(L);
+	displayLineOfStars(L);
 
 	//Tell the calling shell all is well
 	return 0;
