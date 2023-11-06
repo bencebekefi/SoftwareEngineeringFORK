67a96921fe207521efe6b54c384aa39097a722d4
bencebekefi <146179275+bencebekefi@users.noreply.github.com>
2023-11-06 19:12:50 +0000

    work
    work

--- Tasks/03 - Flow Control/307-if-elseif-else/307-if-elseif-else.cpp
+++ Tasks/03 - Flow Control/307-if-elseif-else/307-if-elseif-else.cpp
@@ -32,19 +32,30 @@ int main()
     } while ((level < 0) || (level > 100)); //Repeat until a valid number is entered
 
     // Mutuallly Exclusive Decision Pathway
-    if (level == 0) 
+    if (level == 0)
     {
         cout << "Tank is empty. It needs filling." << endl;
-    } 
-    else if (level <= low)                       // check the level
+    }
+    else if (level <= 5)
     {
-        cout << "Tank is low and needs filling." << endl;
+        cout << "Dangerously low" << endl;
     }
-    else if (level < high) 
+    else if (level <= 30)
     {
-        cout << "Tank has sufficient fuel" << endl;
+        cout << "Low" << endl;
     }
-    else {
-        cout << "Tank is full" << endl;
+    else if (level <= 70)
+    {
+        cout << "Medium" << endl;
     }
+    else if (level <= 95)
+    {
+        cout << "High" << endl;
+    }
+    else (level <= 100);
+    {
+        cout << "Full yaaay" << endl;
+    }
+
 }
+ 
